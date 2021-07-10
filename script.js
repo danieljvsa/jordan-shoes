61def53422aa264e4159a821fb956fb840b6a211
Darko875 <danielviana18@gmail.com>
2021-07-10 14:31:03 +0100

    Animações adicionadas

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '50px',
+    duration: 2000,
+    reset: true
+});
+
+ScrollReveal().reveal('.shop-content', { delay: 400 });
+ScrollReveal().reveal('.card', { delay: 500 });
+ScrollReveal().reveal('.card img', { delay: 600 });
